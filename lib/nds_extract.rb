0f96635ae7ac4393918db33ffebdd1ec730d9f97
Annie Zheng <anniezheng.anniee@gmail.com>
2019-11-12 01:08:50 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -5,12 +5,12 @@ def directors_totals(nds)
   pp nds
   # The Hash result be full of things like "Jean-Pierre Jeunet" => "222312123123"
   result = {}
-  dir_total = 0 
   dir_index = 0 
   while dir_index < nds.length do 
     dir_name = nds[dir_index][:name]
-    mov_index = 0 
     dir_mov = nds[dir_index][:movies]
+    mov_index = 0
+    dir_total = 0 
     while mov_index < dir_mov.length do 
       dir_total += dir_mov[mov_index][:worldwide_gross]
       result[dir_name] = dir_total
